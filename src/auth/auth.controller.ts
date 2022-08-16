d0e6b972878330b56f3a6fe9a40f681d8e3a4c65
Junyeong0218 <hippo2003@naver.com>
2022-08-16 13:54:23 +0900

    2022. 08.16 박준영

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,16 @@
-import { Controller } from '@nestjs/common';
-import { AUTH_API } from 'src/_commons/constants/api-end-point';
+import { Controller, Get } from '@nestjs/common';
+import {
+  AUTH_API,
+  GOOGLE_LOGIN_API,
+} from 'src/_commons/constants/api-end-point';
+import { AuthService } from './auth.service';
 
 @Controller(AUTH_API)
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Get(GOOGLE_LOGIN_API)
+  googleLogin() {
+    this.authService.googleLogin();
+  }
+}
