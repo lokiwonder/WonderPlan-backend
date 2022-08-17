5996c5f7bfdd6234944c2f81ec4db705ff9edf7f
loki <loki@wondermove.net>
2022-08-18 01:08:21 +0900

    2022.08.18. 01:08

--- src/_commons/constants/index.ts
+++ src/_commons/constants/index.ts
@@ -1,3 +1,5 @@
+// todo: config 혹은 시스템 환경 변수로 변경 //
 export const AZURE_SERVER_IP = '20.196.129.128';
+export const AZURE_SERVER_NEST_PORT = '4000';
 export const AZURE_SERVER_DB_PORT = '4717';
 export const MONGODB_URL = `mongodb://${AZURE_SERVER_IP}:${AZURE_SERVER_DB_PORT}`;
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AZURE_SERVER_NEST_PORT } from './_commons/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // description: cors 설정 //
   app.enableCors();
-  // todo: PORT 번호 config 혹은 환경변수로 변경 //
-  await app.listen(4000);
+  await app.listen(AZURE_SERVER_NEST_PORT);
 }
 bootstrap();
