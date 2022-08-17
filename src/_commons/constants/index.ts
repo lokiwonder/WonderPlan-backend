38a664671625d9aeeb2cc858d084e5666bd26fbd
loki <loki@wondermove.net>
2022-08-18 01:05:42 +0900

    2022.08.18. 01:05

--- src/_commons/constants/index.ts
+++ src/_commons/constants/index.ts
@@ -1,1 +1,3 @@
-export const MONGODB_URL = `mongodb://20.196.129.128:4717`;
+export const AZURE_SERVER_IP = '20.196.129.128';
+export const AZURE_SERVER_DB_PORT = '4717';
+export const MONGODB_URL = `mongodb://${AZURE_SERVER_IP}:${AZURE_SERVER_DB_PORT}`;
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // description: cors 설정 //
   app.enableCors();
+  // todo: PORT 번호 config 혹은 환경변수로 변경 //
   await app.listen(4000);
 }
 bootstrap();
